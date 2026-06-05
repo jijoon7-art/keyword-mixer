a2e6832490ad494b314bfe57833ea6ec816246e5
jijoon7-art <jijoon7@gmail.com>
2026-06-05 20:04:03 +0900

    feat: 음식 이상형 월드컵 메뉴 사이드바 추가

---
+++ app/food-worldcup/page.tsx
@@ -0,0 +1,9 @@
+// app/food-worldcup/page.tsx
+export default function FoodWorldCupPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">음식 이상형 월드컵</h1>
+      <p>음식뭐먹지? 오늘점심을 구현해보자!</p>
+    </div>
+  )
+}
